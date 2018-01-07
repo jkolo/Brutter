42dcc0acd9882a40c0e0f2c4e916b5fdeb57831b
Dandry <daniel.andraszewski@gmail.com>
2018-01-07 11:21:51 +0100

    Minor changes in function names.

--- Brutter/crypter.h
+++ Brutter/crypter.h
@@ -1,5 +1,5 @@
 #ifndef _CRYPTER_H_
 #define _CRYPTER_H_
 void secretEncrypt(char *input, char *output);
-void decrypt(char *input, char *key, char *output);
+void encrypt(char *input, char *key, char *output);
 #endif
